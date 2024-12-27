f541c5fb1fd01e65a7d954701ee1e2c73a97e8f8
mannumishra <mannumishra2207@outlook.com>
2024-12-27 10:01:54 +0530

    add chnages

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
                     <h2 className="text-2xl font-extrabold text-[#7B3226]">
                       <Link to="/">
                         MR AND MRS{" "}
-                        <span className="text-[#C5733C]">PERFECT TRIP</span>
+                        <span className="text-[#C5733C]">PERFECT TRIPs</span>
                       </Link>
                     </h2>
                   </div>
